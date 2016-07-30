6dd3abb4727ca005cef90bfaad031c05588ef2cc
Wouter Roozeleer <Wouter Roozeleer>
2016-07-30 16:28:43 +0200

    Added index SPA

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,35 @@
-console.log("dit is een test");
+var homeApp = angular.module('homeApp', ['ngRoute']);
+
+homeApp.config(function ($routeProvider){
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: '../templates/index.html',
+        controller: 'mainCtrl',
+        
+    })
+
+    .when('/docent',{
+        templateUrl: '../templates/docent.html',
+        controller: 'docentCtrl',
+      
+    })
+
+    .when('/student',{
+        templateUrl: '../templates/student.html',
+        controller: 'studentCtrl',
+       
+    });
+
+});
+
+homeApp.controller('mainCtrl', ['$scope', '$log', function($scope, $log) {
+
+}]);
+
+homeApp.controller('docentCtrl', ['$scope', '$log', function($scope, $log) {
+
+}]);
+homeApp.controller('studentCtrl', ['$scope', '$log', function($scope, $log) {
+
+}]);
